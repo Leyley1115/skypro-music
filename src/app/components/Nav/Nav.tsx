28941b1250250b42e0bff72306500215f16ffb2c
Leyley1115 <sophia.pris22@gmail.com>
2026-02-04 11:00:17 +0300

    навигация

--- src/app/components/Nav/Nav.tsx
+++ src/app/components/Nav/Nav.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 import Image from 'next/image';
 import styles from './nav.module.css';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Nav() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
     <nav className={styles.main__nav}>
       <div className={styles.nav__logo}>
@@ -14,29 +18,31 @@ export default function Nav() {
           alt={'logo'}
         />
       </div>
-      <div className={styles.nav__burger}>
+      <div className={styles.nav__burger} onClick={() => setIsNavOpen(!isNavOpen)}>
         <span className={styles.burger__line}></span>
         <span className={styles.burger__line}></span>
         <span className={styles.burger__line}></span>
       </div>
       <div className={styles.nav__menu}>
-        <ul className={styles.menu__list}>
-          <li className={styles.menu__item}>
-            <Link href="#" className={styles.menu__link}>
-              Главное
-            </Link>
-          </li>
-          <li className={styles.menu__item}>
-            <Link href="#" className={styles.menu__link}>
-              Мой плейлист
-            </Link>
-          </li>
-          <li className={styles.menu__item}>
-            <Link href="../signin.html" className={styles.menu__link}>
-              Войти
-            </Link>
-          </li>
-        </ul>
+        {isNavOpen && (
+          <ul className={styles.menu__list}>
+            <li className={styles.menu__item}>
+              <Link href="#" className={styles.menu__link}>
+                Главное
+              </Link>
+            </li>
+            <li className={styles.menu__item}>
+              <Link href="#" className={styles.menu__link}>
+                Мой плейлист
+              </Link>
+            </li>
+            <li className={styles.menu__item}>
+              <Link href="../signin.html" className={styles.menu__link}>
+                Войти
+              </Link>
+            </li>
+          </ul>
+        )}
       </div>
     </nav>
   );
